e692c7eec1f3308aa97a3c1a38f2df754ec1f1f2
serifcolakel <serifcolakel0@gmail.com>
2023-09-26 17:36:43 +0300

    INFO (serif) : initial commit

---
+++ vite.config.ts
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  // eslint-disable-next-line no-console
+  console.log({ env, __dirname, __filename });
+
+  return {
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, 'src/main.tsx'),
+        name: 'ui-components',
+        fileName: (format) => `index.${format}.js`,
+      },
+      rollupOptions: {
+        external: ['react', 'react-dom'],
+        output: {
+          globals: {
+            react: 'React',
+            'react-dom': 'ReactDOM',
+          },
+        },
+      },
+      sourcemap: true,
+      emptyOutDir: true,
+    },
+    plugins: [react(), dts()],
+  };
+});
