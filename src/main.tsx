a0d9dc03fb6e7b6936d11d53a3df6eca4baf1ec4
serifcolakel <serifcolakel0@gmail.com>
2023-11-12 00:41:17 +0300

    Add ConfirmProvider to App render and remove unused import in vite.config.ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import ReactDOM from 'react-dom/client';
+
+import { ConfirmProvider } from './utils/confirm/confirm.provider';
 import App from './App';
+
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ConfirmProvider>
+    <App />
+  </ConfirmProvider>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import dts from 'vite-plugin-dts';
-
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
